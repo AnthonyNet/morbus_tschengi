d452df737e1449cb5578c5c42b57c1a7b0837063
AnthonyZet <zejda.anthony@gmai.com>
2022-11-13 17:24:14 +0100

    Hero section changed

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,17 @@ interface BlogPostFrontmatter {
 interface IndexPosts{
     titleS: string;
 }
+interface Hero{
+        heading2: string;
+        heading1: string;
+        heading2_b: string;
+        blick: string;
+        heading4: string;
+        heading2_c: string;
+}
+
 export type {
     BlogPostFrontmatter,
-    IndexPosts
+    IndexPosts,
+    Hero
 }
