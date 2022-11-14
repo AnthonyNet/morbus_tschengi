dd8ed3c39bce8509dfab1de07af25d33f63610a0
AnthonyZet <zejda.anthony@gmai.com>
2022-11-14 15:52:47 +0100

    blog picutre brno has been changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,13 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
+  }), compress({
+    img: false
   })]
 });
